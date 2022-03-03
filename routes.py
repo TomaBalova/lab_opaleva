89cee23548f183d4704efcfcca207f8d8498cc30
TomaBalova <99354376+TomaBalova@users.noreply.github.com>
2022-03-03 10:14:19 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -33,3 +33,13 @@ def about():
         message='Your application description page.',
         year=datetime.now().year
     )
+
+@route('/Imodel')
+@view('Imodel')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='I am a model',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
